8c7721f29ec91a9577987a2d3986848459a3a9b1
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2025-03-22 13:15:47 +0800

    Add files via upload

--- src/components/widget/StatsWidget.tsx
+++ src/components/widget/StatsWidget.tsx
@@ -14,41 +14,26 @@ export const StatsWidget = ({ data }: { data: BlogStats }) => {
   return (
     <React.StrictMode>
       <WidgetContainer>
-        <div className="items-sketch flex h-full justify-between px-3.5 py-3 md:px-4 md:py-3 lg:px-5 lg:py-4">
-          <div className="flex h-full shrink-0 flex-col justify-between">
-            <div className="inline w-12 origin-bottom-right animate-wave text-4xl lg:text-5xl">
-              👋
-            </div>
-            <div className="flex flex-col gap-y-1.5 text-xl font-semibold sm:text-xl md:text-lg lg:gap-0 lg:gap-y-1.5 lg:text-2xl">
-              <p
-                className={classNames(
-                  colorMap['orange'],
-                  'line-clamp-1 leading-none'
-                )}
-              >
-                {postNum} 篇文章
-              </p>
-              <p
-                className={classNames(
-                  colorMap['pink'],
-                  'line-clamp-1 leading-none'
-                )}
-              >
-                {topicNum} 个话题
-              </p>
-              <p
-                className={classNames(
-                  colorMap['blue'],
-                  'line-clamp-1 leading-none'
-                )}
-              >
-                <PVStats />
-              </p>
-            </div>
+        <div className="items-sketch flex flex-col h-full justify-between px-3.5 py-3 md:px-4 md:py-3 lg:px-5 lg:py-4">
+        <div className="flex h-[72%] w-full flex-row items-start overflow-hidden px-3.5 pt-3.5 md:h-3/5 md:flex-row md:items-center md:justify-center md:space-x-3 md:px-3 md:py-2.5 lg:space-x-4 lg:px-5 lg:py-4">
+        
+           <div className='flex'>
+           <span className="md:text-3xl md:font-medium lg:text-3xl mr-3 text-red-600">PRO</span>
+           <span className="md:text-3xl md:font-medium  font-bold">高级会员计划已推出</span>
+           </div>
+         
+        </div>
+        <div
+          className={classNames(
+            'h-[28%] w-full md:h-2/5',
+            'md:bg-neutral-100 md:dark:bg-neutral-800'
+          )}
+        >
+          <div className="scrollbar-hide flex h-full w-full flex-row items-center justify-between gap-x-1.5 overflow-scroll px-3.5 pt-1 pb-3 font-medium md:justify-start md:gap-x-1 md:px-3 md:py-2 lg:gap-x-3 lg:px-5 lg:py-3 lg:text-sm">
+          <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400" 
+              >立即升级</button>
           </div>
-          <div className="hidden md:block">
-            <StatsGraph timeline={timeline} />
-          </div>
+        </div>
         </div>
       </WidgetContainer>
     </React.StrictMode>
