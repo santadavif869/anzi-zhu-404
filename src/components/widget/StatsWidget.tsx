10fec9fead833947cfa8489f8a85356e276e88d1
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2025-04-01 22:56:32 +0800

    Add files via upload

--- src/components/widget/StatsWidget.tsx
+++ src/components/widget/StatsWidget.tsx
@@ -15,7 +15,7 @@ export const StatsWidget = ({ data }: { data: BlogStats }) => {
     <React.StrictMode>
       <WidgetContainer>
         <div className="items-sketch flex flex-col h-full justify-between px-3.5 py-3 md:px-4 md:py-3 lg:px-5 lg:py-4">
-        <div className="flex h-[72%] w-full flex-row items-start overflow-hidden px-3.5 pt-3.5 md:h-3/5 md:flex-row md:items-center md:justify-center md:space-x-3 md:px-3 md:py-2.5 lg:space-x-4 lg:px-5 lg:py-4">
+        <div className="flex h-[42%] w-full flex-row items-start overflow-hidden px-3.5 pt-3.5  md:flex-row md:items-center md:justify-center md:space-x-3 md:px-3 md:py-2.5 lg:space-x-4 lg:px-5 lg:py-4">
         
            <div className='flex'>
            <span className="md:text-3xl md:font-medium lg:text-3xl mr-3 text-red-600">PRO+</span>
@@ -25,13 +25,15 @@ export const StatsWidget = ({ data }: { data: BlogStats }) => {
         </div>
         <div
           className={classNames(
-            'h-[28%] w-full md:h-2/5',
+            'h-[58%] w-full',
             'md:bg-neutral-100 md:dark:bg-neutral-900'
           )}
         >
-          <div className="scrollbar-hide flex h-full w-full flex-row items-center justify-between gap-x-1.5 overflow-scroll px-3.5 pt-1 pb-3 font-medium md:justify-start md:gap-x-1 md:px-3 md:py-2 lg:gap-x-3 lg:px-5 lg:py-3 lg:text-sm">
-          <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400" 
+          <div className="scrollbar-hide flex h-full w-full flex-col items-center justify-between gap-x-1.5 overflow-scroll px-3.5 pt-1 pb-3 font-medium md:justify-start md:gap-x-1 md:px-3 md:py-2 lg:gap-x-3 lg:px-5 lg:py-3 lg:text-sm">
+          <button onClick={()=>{window.open('https://login.1ho.online')}} type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-red-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400" 
               >立即升级</button>
+              <button onClick={()=>{window.open('https://login.1ho.online')}} type="submit" className="mt-2 flex w-full justify-center rounded-md bg-neutral-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-neutral-800 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400" 
+              >会员登录</button>
           </div>
         </div>
         </div>
